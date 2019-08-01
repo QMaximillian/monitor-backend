7b696b44c68ec513cad21c8e60e3cb155dd8f0d5
QMaximillian <quinnlashinsky@gmail.com>
2019-07-31 23:26:16 -0400

    Update seed files

--- knex/seeds/1564355157_auditions.js
+++ knex/seeds/1564355157_auditions.js
@@ -8,6 +8,7 @@ exports.seed = function(knex) {
         {
           id: "8873c4bc-9a0a-4164-a439-e54ad0eb8b12",
           date: "2019-07-28",
+          show_name: "Mita the Magnificent",
           begin_time: "2019-07-28T08:00:00+0000",
           end_time: "2019-07-28T16:00:00+0000",
           interval: 30,
@@ -21,6 +22,7 @@ exports.seed = function(knex) {
         {
           id: "f4d06d8d-f943-4346-936a-441adbb1a8b1",
           date: "2019-07-29",
+          show_name: "Mary Poppins",
           begin_time: "2019-07-29T08:00:00+0000",
           end_time: "2019-07-29T16:00:00+0000",
           interval: 30,
